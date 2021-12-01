8dba4af16af4753069dad5604f09e5ece3b169df
calidor <victor.calidor@gmail.com>
2021-12-01 19:46:05 -0300

    about & favoritePokemons

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,17 +1,31 @@
-/// let url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/';
-/// url += 'Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
 
 describe('Testa o componente About.js', () => {
   it('A pagina deve conter informações sobre a Pokedex', () => {
-
+    render(<About />);
+    const h2 = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+    expect(h2).toBeInTheDocument();
   });
-  it('A pagina deve conter um H2 com o texto "About Pokédex"', () => {
 
-  });
   it('A pagina deve conter dois parágrafos com texto sobre a Pokédex', () => {
+    render(<About />);
+    const paragraph1 = screen.getByText(
+      /This application simulates a Pokédex, a digital encyclopedia/,
+    );
+    const paragraph2 = screen.getByText(
+      'One can filter Pokémons by type, and see more details for each one of them',
+    );
 
+    expect(paragraph1 && paragraph2).toBeInTheDocument();
   });
-  it('A pagina deve conter uma imagem com a url da variável "url"', () => {
 
+  it('A pagina deve conter uma imagem com a url da variável "url"', () => {
+    render(<About />);
+    const img = screen.getByRole('img', {
+      name: /pokédex/i });
+    expect(img).toHaveAttribute('src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,5 +1,50 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import FavoritePokemons from '../components/FavoritePokemons';
+
 describe('Testa o componente FavoritePokemons.js', () => {
   it('Exibe a mensagem "No favorite pokemon found" se não tiver favoritos', () => {
+    render(<FavoritePokemons pokemons={ [] } />);
+    const noFavorites = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavorites).toBeInTheDocument();
+  });
 
+  it('Exibe os pokemons favoritos se tiver favoritos', () => {
+    const pokemonsArray = [{
+      averageWeight: {
+        measurementUnit: 'kg',
+        value: '8.5',
+      },
+      foundAt: [
+        {
+          location: 'Alola Route 3',
+          map: 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+        },
+        {
+          location: 'Kanto Route 3',
+          map: 'https://cdn2.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
+        },
+        {
+          location: 'Kanto Route 4',
+          map: 'https://cdn2.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
+        },
+        {
+          location: 'Kanto Rock Tunnel',
+          map: 'https://cdn2.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+        },
+      ],
+      id: 4,
+      image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+      name: 'Charmander',
+      summary: 'The flame on its tail shows the strength of its life force.',
+      type: 'Fire',
+    }];
+    render(<FavoritePokemons pokemons={ pokemonsArray } />, { wrapper: BrowserRouter });
+    const charmander = screen.getByText(/Charmander/i);
+    const charmanderType = screen.getByText(/Fire/i);
+    const charmanderImage = screen.getByRole('img', { name: 'Charmander sprite' });
+    expect(charmander && charmanderType && charmanderImage).toBeInTheDocument();
   });
 });
